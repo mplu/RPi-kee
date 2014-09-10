f90726cbf885cf5331fcd390960042c1c9b48ea2
Matthieu <m.pluvinage@gmail.com>
2014-09-10 16:19:51 +0200

    correction on build.sh, added libm and Linux define

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 rm RPi-kee
-gcc main.c llips/llips_contrastdetect.c llips/llips_functions.c llips/llips_general.c llips/llips_io.c llips/llips_motiondetect.c -o RPi-kee -lwiringPi -lpthread
+gcc main.c llips/llips_contrastdetect.c llips/llips_functions.c llips/llips_general.c llips/llips_io.c llips/llips_motiondetect.c -o RPi-kee -lwiringPi -lpthread -lm -DLinux
